7b2b369361fe3c0be50bf638e4d0fb145e68692e
stan900814 <362328861@qq.com>
2019-12-11 01:11:24 +0800

    deploy

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="board">
+      <div class="row">
+        <Box @click="onclick(0,$event)" :n="n"></Box>
+        <Box @click="onclick(1,$event)" :n="n"></Box>
+        <Box @click="onclick(2,$event)" :n="n"></Box>
+      </div>
+      <div class="row">
+        <Box @click="onclick(3,$event)" :n="n"></Box>
+        <Box @click="onclick(4,$event)" :n="n"></Box>
+        <Box @click="onclick(5,$event)" :n="n"></Box>
+      </div>
+      <div class="row">
+        <Box @click="onclick(6,$event)" :n="n"></Box>
+        <Box @click="onclick(7,$event)" :n="n"></Box>
+        <Box @click="onclick(8,$event)" :n="n"></Box>
+      </div>
+    </div>
+    <div>{{result}}</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Box from "./components/Box";
 export default {
-  name: 'app',
+  data() {
+    return {
+      n: 0,
+      arr: Array(9).fill(null),
+      result: ""
+    };
+  },
+  methods: {
+    onclick(i, e) {
+      this.n += 1;
+      this.arr[i] = e;
+      this.judge(this.arr);
+    },
+    judge(arr) {
+      const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+      ];
+      for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (arr[a] && arr[a] === arr[b] && arr[a] === arr[c]) {
+          this.result = `winner：${arr[a]}`;
+        }
+      }
+      return null;
+    }
+  },
   components: {
-    HelloWorld
+    Box
   }
-}
+};
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  display: flex;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  align-items: center;
+  flex-direction: column;
+}
+.row {
+  display: flex;
 }
 </style>
---
+++ src/components/Box.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="suqare" @click="onClick">{{text}}</div>
+</template>
+<script>
+export default {
+  props: ["n"],
+
+  data() {
+    return {
+      text: ""
+    };
+  },
+  methods: {
+    onClick() {
+      if (this.text !== "") {
+        return;
+      } else {
+        this.text = this.n % 2 === 0 ? "x" : "o";
+      }
+
+      this.$emit("click", this.text);
+    }
+  }
+};
+</script>
+<style >
+.suqare {
+  border: 2px solid black;
+  width: 100px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 80px;
+}
+</style>
